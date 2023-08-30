3faa7f8ecdc17d9f2c2c15b58408c71958195c3b
Tim <38327800+Str4ky@users.noreply.github.com>
2023-08-30 22:35:58 +0200

    added responsive design

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,12 +30,12 @@ button.addEventListener('click', async () => {
 
             if(media == true) {
                 const video = data.media.reddit_video.fallback_url;
-                postContent += `<video controls src='${video}' alt='video' id='video' width='500' height='300'></video><br><br>`;
+                postContent += `<video controls src='${video}' alt='video' id='video'></video><br><br>`;
             }
             else {
                 if(mediaURL.includes(".png") || mediaURL.includes(".jpg") || mediaURL.includes(".jpeg") || mediaURL.includes(".gif") || mediaURL.includes(".webp")) {
                     const image = data.url_overridden_by_dest;
-                    postContent += `<img src='${image}' alt='image' id='image' class='thumbnail' onclick="openPopup('${image}')" width='500' height='300' /><br><br>`;
+                    postContent += `<img src='${image}' alt='image' id='image' class='thumbnail' onclick="openPopup('${image}')" /><br><br>`;
                 }
             }
 
