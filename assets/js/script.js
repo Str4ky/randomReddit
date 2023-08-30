3313088b21f8bc2314625eee262af6d8774bf684
Tim <38327800+Str4ky@users.noreply.github.com>
2023-08-30 22:39:26 +0200

    add keyboard control

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,3 +68,54 @@ window.addEventListener('click', (event) => {
         popup.style.display = 'none';
     }
 });
+
+input.addEventListener('keydown', async (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        const subredditName = input.value;
+        const timestamp = Date.now();
+        const jsonUrl = `https://www.reddit.com/r/${subredditName}/random.json?timestamp=${timestamp}`;
+    
+        try {
+            const response = await fetch(jsonUrl);
+            const jsonData = await response.json();
+    
+            if (jsonData.length > 0 && jsonData[0].data.children.length > 0) {
+                const data = jsonData[0].data.children[0].data;
+                const title = data.title;
+                const user = data.author;
+                const description = data.selftext;
+                const media = data.is_video;
+                const mediaURL = data.url_overridden_by_dest;
+                const link = data.permalink;
+                const subreddit = data.subreddit;
+    
+                let postContent = `<p id='infos'><a href='https://reddit.com/r/${subreddit}' target='_blank' id='subreddit'>r/${subreddit}</a> posted by <a href='https://reddit.com/u/${user}' target='_blank' id='user'>u/${user}</a></p>`;
+                postContent += `<h2 id="title">${title}</h2>`;
+    
+                if(description != "") {
+                    postContent += `<p id="description">${description}</p>`;
+                }
+    
+                if(media == true) {
+                    const video = data.media.reddit_video.fallback_url;
+                    postContent += `<video controls src='${video}' alt='video' id='video'></video><br><br>`;
+                }
+                else {
+                    if(mediaURL.includes(".png") || mediaURL.includes(".jpg") || mediaURL.includes(".jpeg") || mediaURL.includes(".gif") || mediaURL.includes(".webp")) {
+                        const image = data.url_overridden_by_dest;
+                        postContent += `<img src='${image}' alt='image' id='image' class='thumbnail' onclick="openPopup('${image}')" /><br><br>`;
+                    }
+                }
+    
+                postContent += `<a href='https://reddit.com${link}' target='_blank' id='link'>Link to post</a>`;
+    
+                post.innerHTML = postContent;
+            } else {
+                post.innerHTML = `<h2 id='error'>No post found</h2>`;
+            }
+        } catch (error) {
+            console.error('Error while parsing JSON datas:', error);
+        }
+    }
+});
